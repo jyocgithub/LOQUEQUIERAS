1a7cd77b33adcdefd0723dd6cbb007f6f388f83f
JYOC <javayotrascosas@gmail.com>
2024-10-18 09:54:30 +0200

    Add files via upload
    Describe lo que acabas de subir....

---
+++ CALCULADORA/src/calculadora/CalsFXMLController.java
@@ -0,0 +1,22 @@
+package calculadora;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author inaki
+ */
+public class CalsFXMLController implements Initializable {
+
+     /**
+      * Initializes the controller class.
+      */
+     @Override
+     public void initialize(URL url, ResourceBundle rb) {
+          // TODO
+     }     
+     
+}
